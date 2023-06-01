07891448d4fd6bce123ddadfc63934f1029f8cb4
russsseeelll <russellmci@hotmail.co.uk>
2023-06-01 19:18:39 +0100

    mail functionality. auto reply

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
-use Illuminate\Validation\Rule;
 
 class ContactController extends Controller
 {
@@ -16,7 +15,15 @@ class ContactController extends Controller
             'message' => ['required', 'string'],
         ]);
 
+        // Send the email to the admin
         Mail::to('admin@rmcinnes.co.uk')->send(new \App\Mail\ContactMail($validatedData));
+
+        // Send the auto-reply to the user
+        $autoReplyDetails = [
+            'name' => $validatedData['name'],
+            'message' => 'Thank you for contacting us. We will get back to you soon.'
+        ];
+        Mail::to($validatedData['email'])->send(new \App\Mail\AutoReplyMail($autoReplyDetails));
 
         return back()->with('message_sent', 'Your message has been sent successfully!');
     }
---
+++ app/Mail/AutoReplyMail.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class AutoReplyMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $details;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($details)
+    {
+        $this->details = $details;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('admin@rmcinnes.co.uk', 'rmcinnes')
+                    ->markdown('emails.autoreply')
+                    ->with([
+                        'name' => $this->details['name'],
+                        'message' => $this->details['message'],
+                    ]);
+    }
+    
+    
+}
+
