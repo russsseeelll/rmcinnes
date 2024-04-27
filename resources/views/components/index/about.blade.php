52b8eaff0235239d5a52174b4643bd413b51c187
russell <russell@russells-MacBook-Pro.local>
2024-04-27 18:58:02 +0200

    ui changes

--- resources/views/components/index/about.blade.php
+++ resources/views/components/index/about.blade.php
@@ -1,25 +1,28 @@
-<section class="about dark" id="about">
-    <div class="container" style="background: rgba(30, 39, 53); border-radius: 20px; padding: 1px;">
-       <div class="row justify-content-center text-center mb-5">
-          <div class="col-12 col-md-8">
-             <h2 class="about-heading fw-bold">About</h2>
-             <p class="about-sub-heading">
-                Hello! I'm Russell, a web developer and home automation artisan from Glasgow. I'm currently employed at a local university, handling their systems and ensuring everything's up to speed. My background includes an HND in Software Development from Glasgow Clyde College, which adds a solid foundation to my everyday work.
-                This is my portfolio website - a small corner of the internet where I get to display my projects. Feel free to explore and see what I've been working on. Enjoy your visit!
-             </p>
-          </div>
-       </div>
+<section class="about dark text-center py-5" id="about">
+    <div class="container" style="background-color: var(--bs-body-bg); color: var(--bs-body-color); overflow: hidden; padding: 50px 0; border: 2px solid var(--theme-colour); border-radius: 15px;">
+        <div class="row justify-content-center text-center mb-5">
+            <div class="col-12 col-md-8">
+                <h2 class="about-heading fw-bold">Russell McInnes</h2>
+                <h1 class="display-3 fw-bold mb-3">Software Engineer</h1>
+                <p class="about-sub-heading lead mb-4">Software engineer with years of experience in Web Development and Automation.</p>
+                <a class="btn btn-lg btn-primary" href="{{ asset('R_McInnes_CV_2024.pdf') }}">View/Download CV</a>
+            </div>
+        </div>
     </div>
- </section>
- <section class="about dark" id="1stitem">
+</section>
+
+
+@include('components.break')
+
+<section class="about dark" id="1stitem">
     <div class="container">
        <div class="row align-items-center justify-content-center">
           <div class="col-12 col-md-5 order-2">
              <div class="d-flex flex-column justify-content-center about-content pe-md-5">
                 <h4 class="fw-bold mb-3">Web-App Development <i class="fas fa-laptop-code"></i></h4>
                 <p class="mb-0">
-                 I developed a strong passion for web development and cloud technology at a young age. 
-                 Drawing from my technical background, I chose to pursue this field and enrolled in college to deepen my knowledge. 
+                 I developed a strong passion for web development and cloud technology at a young age.
+                 Drawing from my technical background, I chose to pursue this field and enrolled in college to deepen my knowledge.
                  Now equipped with a considerable skill set, I have decided to venture into freelance web development.
                 </p>
              </div>
@@ -39,7 +42,7 @@
              <div class="d-flex flex-column justify-content-center about-content ps-md-5">
                 <h4 class="fw-bold mb-3">Home Automation <i class='fas fa-house-user'></i></h4>
                 <p class="mb-0">
-                 I became fascinated with home automation at the start of the COVID-19 pandemic. Spending more time at home made me realize the potential for technology to enhance our daily lives. 
+                 I became fascinated with home automation at the start of the COVID-19 pandemic. Spending more time at home made me realize the potential for technology to enhance our daily lives.
                  I eagerly explored smart devices, IoT technologies, and innovative solutions. This newfound passion has driven me to incorporate home automation into my professional pursuits.
                 </p>
              </div>
