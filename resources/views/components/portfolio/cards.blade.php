fa2df16dbce69e60d141a1fe4fa9cdb1c145e56f
russell <russell@russells-MBP.lan>
2024-05-01 21:23:45 +0100

    changed bootstrap styles

--- resources/views/components/portfolio/cards.blade.php
+++ resources/views/components/portfolio/cards.blade.php
@@ -8,29 +8,32 @@
     <p>No portfolio entries found.</p>
 @else
     @foreach($portfolios as $portfolio)
-        <div class="col-12 col-md-6 col-lg-4 mt-4 d-flex align-items-stretch">
-            <article class="portfolio-post p-3 p-md-4 shadow" style="border-radius: 10px; display: flex; flex-direction: column; height: 100%;">
+        <div class="col-12 col-md-6 col-lg-4 mt-4 d-flex">
+            <article class="portfolio-post p-3 p-md-4 bg-white shadow" style="border-radius: 10px; display: flex; flex-direction: column; height: 100%;">
                 @if ($portfolio->image)
-                    <img src="{{ asset($portfolio->image) }}" alt="{{ $portfolio->title }}" style="height: 200px; width: 100%; object-fit: cover; border-radius: 10px;">
+                    <!-- Ensuring the image fits perfectly within a fixed size, maintaining aspect ratio -->
+                    <div style="height: 200px; width: 100%; overflow: hidden; border-radius: 10px;">
+                        <img src="{{ asset($portfolio->image) }}" alt="{{ $portfolio->title }}" style="height: 100%; width: 100%; object-fit: cover;">
+                    </div>
                 @endif
                 <div class="mt-4 flex-grow-1 d-flex flex-column">
-                    <h4 class="portfolio-title fs-4 mb-2" >{{ $portfolio->title }}</h4>
+                    <h4 class="portfolio-title fs-4 mb-2">{{ $portfolio->title }}</h4>
                     <p class="portfolio-author">
                         <span>At <span>{{ $portfolio->date }}</span></span>
                     </p>
-                    <div>
+                    <div class="portfolio-tags mb-2">
                         @foreach(explode(',', $portfolio->tags) as $tag)
                             <a href="{{ route('portfolio', ['search' => trim($tag)]) }}" class="tech-tag">{{ trim($tag) }}</a>
                         @endforeach
                     </div>
-                    <p class="portfolio-summary mt-3 mb-4" style="flex-grow: 1; opacity: 0.6;">
+                    <p class="portfolio-summary flex-grow-1">
                         {{ $portfolio->summary }}
                     </p>
                     <div class="mt-auto">
-                        <a href="{{ route('portfolio.single', ['portfolio' => $portfolio->slug]) }}" class="btn btn-primary" style="background-color: rgb(13, 110, 253); border-color: rgb(13, 110, 253);">More info</a>
+                        <a href="{{ route('portfolio.single', ['portfolio' => $portfolio->slug]) }}" class="btn portfolio-btn-read-more">More info</a>
                         @auth
-                            <a href="{{ route('portfolio.edit', ['portfolio' => $portfolio->slug]) }}" class="btn btn-info">Edit</a>
-                            <button type="button" class="btn btn-danger delete-portfolio-button" data-bs-toggle="modal" data-bs-target="#delete-modal" data-portfolio-slug="{{ $portfolio->slug }}">Delete</button>
+                            <a href="{{ route('portfolio.edit', ['portfolio' => $portfolio->slug]) }}" class="btn portfolio-btn-read-more">Edit</a>
+                            <button type="button" class="btn portfolio-btn-read-more delete-portfolio-button" data-bs-toggle="modal" data-bs-target="#delete-modal" data-portfolio-slug="{{ $portfolio->slug }}">Delete</button>
                         @endauth
                     </div>
                 </div>
@@ -44,6 +47,7 @@
         <a href="{{ route('portfolio.create')}}" class="btn btn-primary">Add New</a>
     </div>
 @endauth
+
 
 <div class="modal fade" id="delete-modal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
     <div class="modal-dialog">
